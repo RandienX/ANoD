5d56de1e15b3a02cc350b08660da6be1f81048d8
RandienX <delz6969@gmail.com>
2026-07-14 15:08:12 +0200

    reset

--- code/dialogue/DialogueBranch.gd
+++
@@ -1,9 +0,0 @@
-@tool
-class_name DialogueBranch
-extends Resource
-
-## Conditional branch - evaluates condition and jumps to target if true
-@export var conditions: Array[DialogueCondition]
-
-@export var target_label: String = ""      # Jump here if condition is true
-@export var comment: String = ""           # Designer note
--- code/dialogue/custom_effect/dialogue_sink.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-func apply(_effect, _runner):
-	get_tree().current_scene.get_node("TileMap/objects").set_cell(Vector2i(10, 4), 1, Vector2i(8, 2))
